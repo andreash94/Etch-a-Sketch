2a03db9c3981698851c0145b077ac45b461ac4e1
Andreas Hochmuth <hochmuth.andreas@gmail.com>
2022-04-12 13:29:40 -0400

    Got entire grid 16x16 to display

--- javascript.js
+++ javascript.js
@@ -1,27 +1,43 @@
-let gridx = 16;
-let gridy = 16;
+let rowsandcolumns = 16;
+let widthheight = 960/rowsandcolumns
 
 
 function createrows () {
 
-for (var i = 0; i < gridy; i++){
-    console.log(gridy);
+for (var i = 0; i < rowsandcolumns; i++){
     const overall = document.querySelector("#overall")
     const rows = document.createElement('row')
     rows.classList.add(i)
-    rows.textContent = i
-    rows.style.border = "solid 2px"
-    
-
-
-
-    overall.appendChild(rows)
-
-}
-}
-
-function createItems () {
-
-}
+    rows.style.display = "flex"
+    rows.style.height = (widthheight + 'px')
+    //rows.style.border = "solid 2px"
+    overall.appendChild(rows)}}
 
 createrows();
+
+function createitems () {
+    for (z = 0; z<rowsandcolumns; z++) {
+        var targetrow = document.getElementsByTagName("row").item(z)
+
+
+        for (x = 0; x<rowsandcolumns; x++){
+        const items = document.createElement('item')
+        items.classList.add(x)
+        items.style.border = "solid 2px"
+        items.textContent = x
+        items.style.flexGrow = '100'
+        
+       
+
+
+        
+    
+        targetrow.appendChild(items)
+    
+        }
+
+
+
+    }
+}
+createitems();
